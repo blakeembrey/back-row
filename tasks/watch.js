b29896d12ab28d8890b20484cf18f9a9ff491a5d
Blake Embrey <hello@blakeembrey.com>
2015-02-21 09:53:31 +0700

    copy all files, not just assets (because `clean`)

--- tasks/watch.js
+++ tasks/watch.js
@@ -2,7 +2,7 @@ var gulp = require('gulp')
 var browserify = require('./support/browserify')
 
 gulp.task('watch', ['copy'], function () {
-  gulp.watch('app/**/*.{css,html}', ['copy:assets'])
+  gulp.watch('app/**/*.{css,html}', ['copy'])
 
   return browserify(true)
 })
