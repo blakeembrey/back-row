b7e2146982a44545b5903a68a5b97d2529c0741f
Blake Embrey <hello@blakeembrey.com>
2015-04-25 01:19:02 -0300

    update yts domains

--- server/proxy.js
+++ server/proxy.js
@@ -3,11 +3,9 @@ var proxy   = require('./lib/proxy');
 var app     = module.exports = new express.Router();
 
 app.use('/yts', proxy([
-  'https://yts.re/api/',
-  'https://yts.pm/api/',
-  'https://yts.io/api/',
-  'https://yts-proxy.net/api/',
-  'https://yts.wf/api/'
+  'https://yts.to/api/',
+  'https://eqwww.image.yt/api/',
+  'https://yts.io/api/'
 ]));
 
 app.use('/trakt', proxy([
