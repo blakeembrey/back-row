f6e0528c130169c02e4e44a7e3a8a049c3773458
Blake Embrey <hello@blakeembrey.com>
2015-06-09 12:02:30 +1000

    add disabled state to buttons

--- app/components/button.ts
+++ app/components/button.ts
@@ -3,31 +3,42 @@ import chroma = require('chroma-js')
 import { create } from 'react-free-style'
 import { GREEN_SEA } from '../utils/colors'
 
-var Style = create()
+const Style = create()
 
-var BUTTON_STYLE = Style.registerStyle({
+const BUTTON_STYLE = Style.registerStyle({
   padding: '0.6em 1.125em',
   fontSize: '1.1em',
-  cursor: 'pointer',
   color: '#fff',
   borderRadius: '3px',
   backgroundColor: GREEN_SEA,
   border: '1px solid transparent',
   transition: 'border .25s linear, color .25s linear, background-color .25s linear',
   display: 'block',
-  whiteSpace: 'nowrap',
+  whiteSpace: 'nowrap'
+})
+
+const BUTTON_ACTIVE_STYLE = Style.registerStyle({
+  cursor: 'pointer',
 
   '&:hover': {
     backgroundColor: chroma(GREEN_SEA).brighter(5).hex()
   }
 })
 
-class Button extends React.Component<{ children: any }, {}> {
+interface ButtonProps {
+  children: any
+  className: string
+  disabled: boolean
+}
+
+class Button extends React.Component<ButtonProps, {}> {
 
   render () {
     return React.createElement(
       'div',
-      { className: BUTTON_STYLE.className },
+      {
+        className: Style.join(this.props.className, BUTTON_STYLE.className, this.props.disabled ? null : BUTTON_ACTIVE_STYLE.className)
+      },
       this.props.children
     )
   }
